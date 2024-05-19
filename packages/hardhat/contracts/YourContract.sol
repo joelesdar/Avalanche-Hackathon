8a11511f4797f070c6d4608619277637890a3cab
Stovedove <stov3dov3@gmail.com>
2024-05-19 14:19:58 -0500

    Token

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,87 +1,45 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
 
-// Useful for debugging. Remove when deploying to a live network.
-import "hardhat/console.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
+contract YourContract is ERC20, AccessControl {
+    bytes32 constant private MINTER = keccak256(abi.encodePacked("MINTER")); 
+    bytes32 constant private BURNER = keccak256(abi.encodePacked("BURNER")); 
 
-/**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
- */
-contract YourContract {
-	// State Variables
-	address public immutable owner;
-	string public greeting = "Building Unstoppable Apps!!!";
-	bool public premium = false;
-	uint256 public totalCounter = 0;
-	mapping(address => uint) public userGreetingCounter;
+    uint256 constant public INITIALSUPPLY = 100_000_000_000;
+    uint256 public holders;
+    address public airdrop;
 
-	// Events: a way to emit log statements from smart contract that can be listened to by external parties
-	event GreetingChange(
-		address indexed greetingSetter,
-		string newGreeting,
-		bool premium,
-		uint256 value
-	);
+    constructor(address _airdrop, address _minterAddress, address _burnerAddress) ERC20("Action", "ACT") {
+        if(_minterAddress == address(0)) revert ZeroAddressNotAllowed();
+        if(_burnerAddress == address(0)) revert ZeroAddressNotAllowed();
+        _grantRole(MINTER, _minterAddress);  
+        _grantRole(BURNER, _burnerAddress); 
+        airdrop = _airdrop;
+        _mint(airdrop, INITIALSUPPLY*10**decimals());
+    }
 
-	// Constructor: Called once on contract deployment
-	// Check packages/hardhat/deploy/00_deploy_your_contract.ts
-	constructor(address _owner) {
-		owner = _owner;
-	}
+    error ZeroAddressNotAllowed();
+    
+    function mint(uint256 amount) onlyRole(MINTER) public { 
+        _mint(airdrop, amount);
+    }
 
-	// Modifier: used to define a set of rules that must be met before or after a function is executed
-	// Check the withdraw() function
-	modifier isOwner() {
-		// msg.sender: predefined variable that represents address of the account that called the current function
-		require(msg.sender == owner, "Not the Owner");
-		_;
-	}
+    function burn(uint256 amount) onlyRole(BURNER) public {
+        _burn(airdrop, amount*10**decimals());
+    }
 
-	/**
-	 * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-	 *
-	 * @param _newGreeting (string memory) - new greeting to save on the contract
-	 */
-	function setGreeting(string memory _newGreeting) public payable {
-		// Print data to the hardhat chain console. Remove when deploying to a live network.
-		console.log(
-			"Setting new greeting '%s' from %s",
-			_newGreeting,
-			msg.sender
-		);
+    function _update(address from, address to, uint256 value) internal override virtual { 
+        if(balanceOf(to) == 0) {
+            holders += 1;
+        }
+        if(balanceOf(from) == value) {
+            holders -= 1;
+        }
+        super._update(from, to, value);
+    }
 
-		// Change state variables
-		greeting = _newGreeting;
-		totalCounter += 1;
-		userGreetingCounter[msg.sender] += 1;
 
-		// msg.value: built-in global variable that represents the amount of ether sent with the transaction
-		if (msg.value > 0) {
-			premium = true;
-		} else {
-			premium = false;
-		}
-
-		// emit: keyword used to trigger an event
-		emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, msg.value);
-	}
-
-	/**
-	 * Function that allows the owner to withdraw all the Ether in the contract
-	 * The function can only be called by the owner of the contract as defined by the isOwner modifier
-	 */
-	function withdraw() public isOwner {
-		(bool success, ) = owner.call{ value: address(this).balance }("");
-		require(success, "Failed to send Ether");
-	}
-
-	/**
-	 * Function that allows the contract to receive ETH
-	 */
-	receive() external payable {}
 }
